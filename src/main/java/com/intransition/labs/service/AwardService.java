e8ba61ffa151b8fc55c20fac90ec27de779b155e
shadow1230001 <vlyubin@list.ru>
2017-03-12 17:46:22 +0300

    some fixes from kurator

--- src/main/java/com/intransition/labs/service/AwardService.java
+++ src/main/java/com/intransition/labs/service/AwardService.java
@@ -6,11 +6,32 @@ import com.intransition.labs.domain.user.User;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Award Service.
+ */
+
 public interface AwardService {
+    /**
+     *
+     * @param type Passes the type indicator enum
+     * @return Returns an instance of bean
+     */
 
     Award getAward(AwardType type);
 
+    /**
+     *
+     * @param awardsToSort 
+     * @return
+     */
     List<Award> sortAwardsByAwardedTime(Set<Award> awardsToSort);
+
+    /**
+     * 
+     * @param user
+     * @param type
+     * @return
+     */
 
     Award giveUserAward(User user, AwardType type);
 
